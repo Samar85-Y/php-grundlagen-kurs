3fec0e1c52e7f4d1d12634f979e620c0b22c108f
Samar85-Y <samar85yousef@gmail.com>
2025-11-13 12:56:24 +0000

    Formular  beisple hinzugefügt

--- php_kurs_woche1/materialien/beispiele/04_arrays_formulare/beispiel_formular.php
+++ php_kurs_woche1/materialien/beispiele/04_arrays_formulare/beispiel_formular.php
@@ -1,6 +1,20 @@
 <?php
 declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+//echo '<pre>', print_r( $_POST, false ), '</pre>';
 
+$name = $_POST['fullname'] ?? '';
+$msg = $_POST['msg'] ?? '';
+$error = '';
+
+// Prüfen ob diese Dateiüber ein Formular versendet wurde
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+//Prüfe ob alle Felder ausgefüllt wurden
+  if( empty(trim($name)) === '' || empty(trim($msg)) === ''){
+    $error = 'Bitte alle Felder ausfüllen';
+  }
+}
 ?>
 <!doctype html>
 <html lang="de">
@@ -13,7 +27,28 @@ declare(strict_types=1);
 <body>
   <header><h1>Formular & Validierung</h1></header>
   <main class="container">
+    <?php if($error): ?>
+    <p class="alert"><?= htmlspecialchars($error) ?><?php endif ?></p>
+    <form action="<?= $_SERVER['SCRIPT_NAME'] ?>" method="post" class="card">
     
+    <label for="">Name:
+      <input type="text" name="fullname" value="<?= htmlspecialchars($name)?>">
+    </label>
+
+    <label for="">
+      Nachricht:
+      <textarea name="msg" rows="4"><?= nl2br(htmlspecialchars($msg)) ?></textarea>
+    </label>
+
+    <button type="submit">Senden</button>
+    </form>
+    <?php
+      if(isset($_POST) && !$error) :  
+    ?>
+    <hr>
+    <h2>Ausgabe</h2>
+    <p><b><?= htmlspecialchars($name) ?>:</b><?= nl2br(htmlspecialchars($msg)) ?></p>
+    <?php endif ?>
   </main>
 </body>
 </html>
